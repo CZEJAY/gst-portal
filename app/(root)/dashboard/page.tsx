c20fce65904fa22824bef2630ae269f1b9364591
CZEJAY <calebjimmy67@gmail.com>
2024-06-12 19:54:31 +0100

    table

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -7,17 +7,17 @@ import prismadb from '@/lib/prisma'
 
 const page = async  () => {
   const session = await getSession()
-  const user = await prismadb.registrars.findMany({
-    where: {
-      id: session?.user?.id
-    },
-    include: {
-      students: true
-    },
-    orderBy: {
-      createdAt: "desc"
-    }
-  })
+  // const user = await prismadb.registrars.findMany({
+  //   where: {
+  //     id: session?.user?.id
+  //   },
+  //   include: {
+  //     students: true
+  //   },
+  //   orderBy: {
+  //     createdAt: "desc"
+  //   }
+  // })
   return (
     <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
       <NewReg />
