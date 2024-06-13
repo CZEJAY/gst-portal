3a55922d1b4a3ab279648a981d8a0cc88dd4312d
CZEJAY <calebjimmy67@gmail.com>
2024-06-13 22:39:59 +0100

    tree

--- components/FormInputs/SelectInputCourse.tsx
+++ components/FormInputs/SelectInputCourse.tsx
@@ -45,9 +45,9 @@ const SelectInputCourse: React.FC<SelectInputCourseProps> = ({
         <select
           {...register(name)}
           id={name}
-          multiple={multiple}
+          multiple={multiple} 
           name={name}
-          className="block min-w-full rounded-md h-10 border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-slate-500 focus:ring-inset sm:max-w-xs sm:text-sm sm:leading-6"
+          className="block min-w-full rounded-md h-10 border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-slate-500 focus:ring-inset sm:max-w-xs text-sm sm:leading-6"
           onChange={handleChange}
         >
           {options.map((option, i) => (
