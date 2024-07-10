4945a57539ac80e2a5febbc692d3f4b097173389
CZEJAY <calebjimmy67@gmail.com>
2024-07-10 17:00:09 +0100

    style

--- app/closed/page.tsx
+++ app/closed/page.tsx
@@ -9,12 +9,12 @@ const Modal = () => {
         <div className="">
             <img src="/uniuyo-logo.png" alt="logo" className='mx-auto w-32' />
         </div>
-        <h2 className="text-xl font-semibold text-center mb-10">Biometric Portal Maintenance</h2>
-        <p className="mb-4">
+        <h2 className="text-2xl font-semibold text-center mb-10">Biometric Portal Maintenance</h2>
+        <p className="mb-4 text-center font-semibold text-lg">
           The Biometric portal is currently under maintenance. Please check back later. We apologize for any inconvenience caused.
         </p>
         <button
-          className="px-4 py-2 bg-orange-900 text-white rounded hover:bg-orange-600"
+          className="px-4 tracking-wider py-2 bg-orange-900 text-white rounded hover:bg-orange-600"
         >
           Thank You!
         </button>
