5cce37f0b8a82a24633b5f53c785d36d5a9914f9
CZEJAY <calebjimmy67@gmail.com>
2024-08-24 11:46:56 +0100

    Format Typography

--- components/shared/Print.tsx
+++ components/shared/Print.tsx
@@ -58,7 +58,7 @@ export default function Print({ StudentData }: { StudentData?: students }) {
 
   return (
     <div className="w-screen justify-center h-screen flex items-start">
-      <div className="fixed bottom-0 flex items-center justify-between gap-4">
+      <div className="fixed top-0 z-30 flex items-center justify-between gap-4">
         <button
           hidden={isPrinting}
           type="button"
@@ -87,7 +87,7 @@ export default function Print({ StudentData }: { StudentData?: students }) {
               className="w-32"
             />
             <h1 className="text-center text-xl font-bold">
-              1st Semester GST Supplementary Exams Slip
+              2nd Semester GST Exams Slip
             </h1>
           </div>
           <div className="self-start gap-5 flex items-start">
@@ -101,33 +101,33 @@ export default function Print({ StudentData }: { StudentData?: students }) {
                 className="object-cover w-full h-full transform scale-105 "
               />
             </div>
-            <div className="flex gap-2 items-start flex-col">
+            <div className="flex  items-start flex-col">
               <p className="text-xl text-center font-bold tracking-wider">
                 {formData?.surName?.toUpperCase()},{" "}
                 {formData?.firstName?.toUpperCase()}{" "}
                 {formData?.otherName?.toUpperCase()}
               </p>
-              <p className="text-xl font-bold w-full flex gap-2 items-center">
+              <p className="text-md font-bold w-full flex gap-2 items-center">
                 Matric Number:{" "}
-                <span className="font-bold ml-auto text-lg text-blue-900">
+                <span className="font-bold text-blue-900">
                   {formData?.matricNumber || "N/A"}
                 </span>
               </p>
-              <p className="text-xl font-bold w-full gap-2 flex items-center">
+              <p className="text-md font-bold w-full gap-2 flex items-center">
                 Email:{" "}
-                <span className="font-bold text-lg  text-blue-900">
+                <span className="font-bold  text-blue-900">
                   {formData?.email || "N/A"}
                 </span>
               </p>
-              <p className="text-xl font-bold w-full gap-2 flex items-center">
+              <p className="text-md font-bold w-full gap-2 flex items-center">
                 Phone:{" "}
-                <span className="font-bold text-lg text-blue-900">
+                <span className="font-bold text-blue-900">
                   {formData?.phone || "N/A"}
                 </span>
               </p>
-              <p className="text-xl font-bold w-full gap-2 flex items-center">
+              <p className="text-md font-bold w-full gap-2 flex items-center">
                 Gender:{" "}
-                <span className="font-bold text-lg text-blue-900">
+                <span className="font-bold text-blue-900">
                   {formData?.gender || "N/A"}
                 </span>
               </p>
@@ -150,19 +150,19 @@ export default function Print({ StudentData }: { StudentData?: students }) {
               <div className="col-span-1 flex flex-col gap-8">
                 <div className="">
                   <p className="text-sm font-bold">Faculty</p>
-                  <p className="text-xl font-bold text-blue-900">
+                  <p className="text-md font-bold text-blue-900">
                     {formData?.faculty || "N/A"}
                   </p>
                 </div>
                 <div className="">
                   <p className="text-sm font-bold">Current Level</p>
-                  <p className="text-xl font-bold text-blue-900">
+                  <p className="text-md font-bold text-blue-900">
                     {formData?.level || "N/A"}
                   </p>
                 </div>
                 <div className="">
                   <p className="text-sm font-bold">GST Courses</p>
-                  <p className="text-xl font-bold text-blue-900">
+                  <p className="text-md font-bold text-blue-900">
                     {/* convert the comma separated strings to an array */}
                     <ol className="">
                       <li className="list-item">
@@ -176,13 +176,13 @@ export default function Print({ StudentData }: { StudentData?: students }) {
               <div className="col-span-1 flex flex-col gap-8">
                 <div className="">
                   <p className="text-sm font-bold">Department</p>
-                  <p className="text-lg font-bold text-blue-900">
+                  <p className="text-md font-bold text-blue-900">
                     {formData?.department || "N/A"}
                   </p>
                 </div>
                 <div className="">
                   <p className="text-sm font-bold">Application Date</p>
-                  <p className="text-lg font-bold text-blue-900">
+                  <p className="text-md font-bold text-blue-900">
                     {(formData &&
                       format(
                         new Date(formData?.createdAt),
